8da1cb61a838864f4342f02d607e01f645240d31
themaras <themaras3@gmail.com>
2026-09-18 01:02:13 +0300

    Fix Java and Kotlin JVM target mismatch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,13 @@ android {
         versionCode = 1
         versionName = "0.1"
     }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
